9c442420b39b4bef0591962a43927fa955393f07
NarendraDasari <NarendraDasari.dotnet@gmail.com>
2023-01-16 00:51:58 -0600

    initial commit for java programming basics

---
+++ javabasics/src/syntax/ProgramSyntax.java
@@ -0,0 +1,20 @@
+package syntax;
+
+public class ProgramSyntax {
+
+	public static void main(String[] args) {
+			System.out.println("Hello World");
+			System.out.println("how r u");
+			greetUser();
+	}
+	
+	public static void greetUser()
+	{
+		System.out.println("Welcome User");
+		System.out.println("hai");
+		System.out.println("how do you do");
+		System.out.println("what r u doing");
+		
+	}
+
+}

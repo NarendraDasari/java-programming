ad57961be96171181ca505d93c1e5dc95c0be0ca
NarendraDasari <NarendraDasari.dotnet@gmail.com>
2023-01-18 20:26:57 -0600

    my goal is to greet the user.I have written 4 functions to accept firstname, lastname, middlename andprefix as input parameter to achieve my goal.I acheived my goal.

--- javabasics/src/syntax/ProgramSyntax.java
+++ javabasics/src/syntax/ProgramSyntax.java
@@ -9,13 +9,32 @@ public class ProgramSyntax {
 			String userName="ggghgghjhecommeghgj13";
 			String firstName="Chaitanya";
 			String lastName="Nanduri";
+			String middleName="Babu";
+			String prefix ="Mrs";
 	        String fullName=firstName+ " " +lastName;
 			greetUser(fullName);
+			decorateAndgreetUser(firstName, lastName);
+			greetUserwithMiddlename(firstName, middleName, lastName);
+			honor(prefix, firstName, middleName, lastName);
 	}
 	
 	public static void greetUser(String name)
 	{
 		System.out.println("Welcome "+ name);
 	}
+	
+	public static void decorateAndgreetUser(String firstName,String lastName)
+	{
+		System.out.println("Welcome "+ firstName + " "+ lastName +".");
+	}
+	public static void greetUserwithMiddlename(String firstName,String middleName,String lastName)
+	{
+		System.out.println("Welcome "+ firstName + " " + middleName +" " + lastName +".");
+	}
+	public static void honor(String prefix,String firstName,String middleName,String lastName)
+	{
+		System.out.println("Welcome " + prefix +" " + firstName + " " + middleName + " "  + lastName +".");
+	}
+	
 	
 }

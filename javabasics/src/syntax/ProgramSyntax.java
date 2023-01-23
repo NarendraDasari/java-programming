4dd98d04e3cba1fd37e0e662c001b7bb5613ba24
NarendraDasari <NarendraDasari.dotnet@gmail.com>
2023-01-22 22:03:56 -0600

    capturing user inputs using SCANNER class.

--- javabasics/src/syntax/ProgramSyntax.java
+++ javabasics/src/syntax/ProgramSyntax.java
@@ -1,30 +1,69 @@
 package syntax;
+import java.util.Scanner;
+
 import calculator.*;
 
 public class ProgramSyntax {
 
 	public static void main(String[] args) {
-			System.out.println("Hello World");
-			System.out.println("how r u");
+			//System.out.println("Hello World");
+			//System.out.println("how r u");
 			
-			String userName="ggghgghjhecommeghgj13";
-			String firstName="Chaitanya";
-			String lastName="Nanduri";
-			String middleName="Babu";
-			//String prefix ="Mrs";
-	        String fullName=firstName+ " " +lastName;
-			//greetUser(fullName);
-			//decorateAndgreetUser(firstName, lastName);
-			//greetUserwithMiddlename(firstName, middleName, lastName);
-			//honor(prefix, firstName, middleName, lastName);
-	        
-	        greetUser(firstName, lastName);
-	        
-	        calculatorClass calc=new calculatorClass();   
-	        calc.add(50,50);
-	        calc.sub(30,10);
-	        calc.multiply(2,5);
-	        calc.divide(15,5);
+			
+			Scanner input=new Scanner(System.in);
+			System.out.println("Enter your FirstName");
+			String firstName=input.next();
+			System.out.println("Enter your LastName");
+			String lastName=input.next();
+			
+			greetUser(firstName,lastName);
+			
+			System.out.println("the below two input lines is for ADDITION");
+			System.out.println("Enter a value");
+			int a=input.nextInt();
+			System.out.println("Enter b value");
+			int b=input.nextInt();
+			
+			
+			calculatorClass calc=new calculatorClass();
+			calc.add(a, b);
+			
+			
+			 System.out.println("the below two lines is for SUBSTRACTION");
+		     System.out.println("Enter x value");
+		     int x=input.nextInt();
+		     System.out.println("Enter y value");
+		     int y=input.nextInt();
+		      
+		     calc.sub(x, y);
+		      
+		      System.out.println("the below two lines is for MULTIPLICATION");
+			  System.out.println("Enter p value");
+			  int p=input.nextInt();
+			  System.out.println("Enter q value");
+			  int q=input.nextInt();
+			  
+			   calc.multiply(p, q);
+			   
+			   System.out.println("the below two lines is for DIVISION");
+			   System.out.println("Enter m value");
+			   int m=input.nextInt();
+			   System.out.println("Enter n value");
+			   int n=input.nextInt();
+			    
+			   calc.divide(m, n);
+			   
+			
+			
+			
+			
+			
+			
+	        //calculatorClass calc=new calculatorClass();   
+	        //calc.add(50,50);
+	        //calc.sub(30,10);
+	        //calc.multiply(2,5);
+	        //calc.divide(15,5);
 	        
 	        
 	}

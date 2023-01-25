df22b2fcfcba49de5655ad33482d5336e487c883
NarendraDasari <NarendraDasari.dotnet@gmail.com>
2023-01-24 23:01:51 -0600

    imported external jar for vehical entity.

---
+++ javabasics/src/calculator/calculatorClass.java
@@ -0,0 +1,28 @@
+package calculator;
+
+public class calculatorClass {
+
+	public void add(int a,int b)
+	{
+
+		int c=a+b;
+		
+		System.out.println("The sum of two numbers is " + c);
+	}
+	
+	public void sub(int a,int b)
+	{
+		int c=a-b;
+		System.out.println("The difference of two numbers is " + c);
+		}
+    public void multiply(int a,int b)
+    {
+    	int c=a*b;
+    	System.out.println("The product of two numbers is " + c);
+    }
+    public void divide(int a,int b)
+    {
+    	int c=a/b;
+	     System.out.println("The  quoitent of two numbers is " + c);
+    }
+}

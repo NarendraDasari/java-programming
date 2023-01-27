4b9bc3b99e3371c16ef79f4746d8f69fc2961ffd
NarendraDasari <NarendraDasari.dotnet@gmail.com>
2023-01-26 21:27:33 -0600

    code refactoring-seperated blocks of code from main method into seperate functions.

--- javabasics/src/syntax/ProgramSyntax.java
+++ javabasics/src/syntax/ProgramSyntax.java
@@ -2,6 +2,9 @@ package syntax;
 //import java.util.Scanner;
 //import Models.*;
 
+import java.util.Scanner;
+
+import calculator.calculatorClass;
 //import calculator.*;
 import dto.models.Vehical;
 
@@ -11,81 +14,79 @@ public class ProgramSyntax {
 			//System.out.println("Hello World");
 			//System.out.println("how r u");
 			
-			
-//			Scanner input=new Scanner(System.in);
-//			System.out.println("Enter your FirstName");
-//			String firstName=input.nextLine();
-//			System.out.println("Enter your LastName");
-//			String lastName=input.nextLine();
-//			
-//			greetUser(firstName,lastName);
-//			
-//			System.out.println("the below two input lines is for ADDITION");
-//			System.out.println("Enter a value");
-//			int a=input.nextInt();
-//			System.out.println("Enter b value");
-//			int b=input.nextInt();
-//			
-//			
-//			calculatorClass calc=new calculatorClass();
-//			calc.add(a, b);
-//			
-//			
-//			 System.out.println("the below two lines is for SUBSTRACTION");
-//		     System.out.println("Enter x value");
-//		     int x=input.nextInt();
-//		     System.out.println("Enter y value");
-//		     int y=input.nextInt();
-//		      
-//		     calc.sub(x, y);
-//		      
-//		      System.out.println("the below two lines is for MULTIPLICATION");
-//			  System.out.println("Enter p value");
-//			  int p=input.nextInt();
-//			  System.out.println("Enter q value");
-//			  int q=input.nextInt();
-//			  
-//			   calc.multiply(p, q);
-//			   
-//			   System.out.println("the below two lines is for DIVISION");
-//			   System.out.println("Enter m value");
-//			   int m=input.nextInt();
-//			   System.out.println("Enter n value");
-//			   int n=input.nextInt();
-//			    
-//			   calc.divide(m, n);
-//			   
-			
-			
-			
-			
-			
-			
+			startGreetingtheUser();
+			calculator();
+			vechicalHistory();
+					
 	        //calculatorClass calc=new calculatorClass();   
 	        //calc.add(50,50);
 	        //calc.sub(30,10);
 	        //calc.multiply(2,5);
-	        //calc.divide(15,5);
-//			   Vehical beh=new Vehical();
-//			   beh.make = "Honda";
-//			   System.out.println("The name of the vehical is" + beh.make);
-			   
-			   Vehical obj = new Vehical();
-			   
+	        //calc.divide(15,5);			   
 			  
-			   String color=obj.getColor();
-			   System.out.println("the color of the vehical is" + color);
-			   
-			   String make=obj.getMake();
-			   System.out.println("the make of the vehical is" +make);
-			   String model=obj.getModel();
-			   System.out.println("the model of the vehical is" +model);
 			   //obj.getMake();
 			  // obj.getModel()
-	        
-	        
 	}
-	
+	public static void startGreetingtheUser()
+	{
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter your FirstName");
+		String firstName=input.nextLine();
+		System.out.println("Enter your LastName");
+		String lastName=input.nextLine();
+		greetUser(firstName,lastName);
+		
+	}
+	public static void calculator()
+	{
+		Scanner input=new Scanner(System.in);
+		System.out.println("the below two input lines is for ADDITION");
+		System.out.println("Enter a value");
+		int a=input.nextInt();
+		System.out.println("Enter b value");
+		int b=input.nextInt();
+		
+		
+		calculatorClass calc=new calculatorClass();
+		calc.add(a, b);
+		
+		
+		 System.out.println("the below two lines is for SUBSTRACTION");
+	     System.out.println("Enter x value");
+	     int x=input.nextInt();
+	     System.out.println("Enter y value");
+	     int y=input.nextInt();
+	      
+	     calc.sub(x, y);
+	      
+	      System.out.println("the below two lines is for MULTIPLICATION");
+		  System.out.println("Enter p value");
+		  int p=input.nextInt();
+		  System.out.println("Enter q value");
+		  int q=input.nextInt();
+		  
+		   calc.multiply(p, q);
+		   
+		   System.out.println("the below two lines is for DIVISION");
+		   System.out.println("Enter m value");
+		   int m=input.nextInt();
+		   System.out.println("Enter n value");
+		   int n=input.nextInt();
+		    
+		   calc.divide(m, n);
+	}
+	public static void vechicalHistory()
+	{
+		 Vehical obj = new Vehical();
+		  
+		   String color=obj.getColor();
+		   System.out.println("the color of the vehical is" + color);
+		   
+		   String make=obj.getMake();
+		   System.out.println("the make of the vehical is" +make);
+		   String model=obj.getModel();
+		   System.out.println("the model of the vehical is" +model);
+	}
 	public static void greetUser(String username)
 	{
 		System.out.println("Welcome "+ username);
